522f21a98cf11b3cf3936596c10c66655d241f41
Danny McGee <dannymcgee@gmail.com>
2023-01-18 15:15:02 -0500

    klipper-script builtin filters

--- packages/klipper-script/src/lib/repository/identifier.ts
+++ packages/klipper-script/src/lib/repository/identifier.ts
@@ -5,6 +5,10 @@ import { IDENT } from "../common";
 export const identifier: TMGrammarScope = {
 	patterns: [
 		{
+			name: "entity.name.function.filter.klipper-script",
+			match: /\b(abs|attr|batch|capitalize|center|default|dictsort|escape|filesizeformat|first|float|forceescape|format|groupby|indent|int|join|last|length|list|lower|map|max|min|pprint|random|reject|rejectattr|replace|reverse|round|safe|select|selectattr|slice|sort|string|striptags|sum|title|tojson|truncate|unique|upper|urlencode|urlize|wordcount|wordwrap|xmlattr)\b/,
+		},
+		{
 			match: regex`/\b(${IDENT})\s*(?=\()/`,
 			captures: {
 				1: { name: "entity.name.function.klipper-script" },
